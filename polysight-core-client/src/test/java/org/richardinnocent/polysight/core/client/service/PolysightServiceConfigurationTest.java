0f522a0e967889fa9320fded8145de0b07fbf5f8
RichardInnocent <richardinnocent@sky.com>
2020-05-22 22:50:08 +0100

    Removed redundant parameter from PolysightServiceConfiguration constructor, and incremented the version number ready for release.

--- polysight-core-client/src/test/java/org/richardinnocent/polysight/core/client/service/PolysightServiceConfigurationTest.java
+++ polysight-core-client/src/test/java/org/richardinnocent/polysight/core/client/service/PolysightServiceConfigurationTest.java
@@ -1,12 +1,10 @@
 package org.richardinnocent.polysight.core.client.service;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
 
 import nl.jqno.equalsverifier.EqualsVerifier;
 import nl.jqno.equalsverifier.Warning;
 import org.junit.Test;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 
 public class PolysightServiceConfigurationTest {
 
@@ -14,10 +12,8 @@ public class PolysightServiceConfigurationTest {
   public void testSettingValuesFromConstructor() {
     String baseUri = "https://myservice.com";
     ServiceVersion version = new MajorOnlyServiceVersion(3);
-    RestTemplateBuilder templateBuilder = mock(RestTemplateBuilder.class);
 
-    PolysightServiceConfiguration config =
-        new PolysightServiceConfiguration(baseUri, version, templateBuilder);
+    PolysightServiceConfiguration config = new PolysightServiceConfiguration(baseUri, version);
     assertEquals(baseUri, config.getBaseUri());
     assertEquals(version, config.getVersion());
   }
@@ -26,10 +22,9 @@ public class PolysightServiceConfigurationTest {
   public void testUriHasTrailingForwardSlashRemovedIfPresent() {
     String baseUri = "https://myservice.com";
     ServiceVersion version = new MajorOnlyServiceVersion(3);
-    RestTemplateBuilder templateBuilder = mock(RestTemplateBuilder.class);
 
     PolysightServiceConfiguration config =
-        new PolysightServiceConfiguration(" " + baseUri + "/ ", version, templateBuilder);
+        new PolysightServiceConfiguration(" " + baseUri + "/ ", version);
     assertEquals(baseUri, config.getBaseUri());
     assertEquals(version, config.getVersion());
   }
